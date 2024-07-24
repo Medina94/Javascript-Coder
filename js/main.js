b39f121b228170a768530d23f36925aa5ff775fc
Cristian Medina <cristian.medina94@gmail.com>
2024-07-23 22:39:22 -0300

    creacion de repo y primer avance de js

---
+++ js/main.js
@@ -0,0 +1,79 @@
+const frutas = [{producto: 'manzana', stock: 10}, {producto: 'pera', stock: 10}, {producto: 'uvas', stock: 10}];
+const almacen = [{producto: 'arroz', stock: 10}, {producto: 'fideos', stock: 10}, {producto: 'galletas', stock: 10}];
+const limpieza = [{producto: 'shampoo', stock: 10}, {producto: 'cepillo de dientes', stock: 10}, {producto: 'desodorante', stock: 10}];
+
+// Valido la cantidad ingresada contra el stock que se define por cada producto
+const validarStock = (cant, stock) =>{
+    return stock >= cant;
+}
+
+// Pido por consola el producto a agregar y lo inserto en el array 'carrito'
+const agregarACarrito = (opcion) => {
+    let seguirAgregando = true;
+    let productoElegido = 0;
+    let cantidadElegida = 0;
+    switch(opcion){
+        case 1:{
+            productoElegido = parseInt(prompt('Elija un producto:\n1 - manzana\n2 - pera\n3 - uvas\n4 - Volver\n0 - Terminar'));
+            cantidadElegida = parseInt(prompt('Elija una cantidad: '));
+            // valido para no agregar más que el stock
+            if(validarStock(cantidadElegida, frutas[productoElegido].stock)){
+                // agrego al carrito productos de frutas
+                carrito.push({producto: frutas[productoElegido].producto, cantidadElegida});
+                console.log('Se agrego ' + frutas[productoElegido].producto + ' al carrito...Tenés ' + carrito.length + ' productos en tu carrito.');
+            }else{
+                alert('Stock insuficiente!!');
+            }
+            break;
+        }
+        case 2:{
+            productoElegido = parseInt(prompt('Elija un producto:\n1 - arroz\n2 - fideos\n3 - galletas\n4 - Volver\n0 - Terminar'));
+            cantidadElegida = parseInt(prompt('Elija una cantidad: '));
+            // valido para no agregar más que el stock
+            if(validarStock(cantidadElegida, almacen[productoElegido].stock)){
+                // agrego al carrito productos de almacen
+                carrito.push({producto: almacen[productoElegido].producto, cantidadElegida});
+                console.log('Se agrego ' + almacen[productoElegido].producto + ' al carrito...Tenés ' + carrito.length + ' productos en tu carrito.');
+            }else{
+                alert('Stock insuficiente!!');
+            }
+            break;
+        }
+        case 3:{
+            productoElegido = parseInt(prompt('Elija un producto:\n1 - shampoo\n2 - cepillo de dientes\n3 - desodorante\n4 - Volver\n0 - Terminar'));
+            cantidadElegida = parseInt(prompt('Elija una cantidad: '));
+            // valido para no agregar más que el stock
+            if(validarStock(cantidadElegida, limpieza[productoElegido].stock)){
+                // agrego al carrito productos de limpieza
+                carrito.push({producto: limpieza[productoElegido].producto, cantidadElegida});
+                console.log('Se agrego ' + limpieza[productoElegido].producto + ' al carrito...Tenés ' + carrito.length + ' productos en tu carrito.');
+            }else{
+                alert('Stock insuficiente!!');
+            }
+            break;
+        }
+        case 4:
+            // lo manejo SOLO para salir del menu y permitir seguir agregando
+            console.log('Vuelviendo al menu...');
+            break;
+        case 0:{
+            seguirAgregando = false;
+            break;
+        }
+        default:
+            console.log('opcion incorrecta. Selecciona una de las opciones indicadas...');
+    }
+    return seguirAgregando;
+}
+
+// carrito de compras
+let carrito = [];
+let agregar = true;
+
+const producto = 0;
+const cantidad = 0;
+
+while(agregar){
+    const categoria = parseInt(prompt('Agrega productos a tu carrito de compras:\n1 - Frutas\n2 - Almacen\n3 - Limpieza\n0 - Salir'));
+    agregar = agregarACarrito(categoria);
+}
