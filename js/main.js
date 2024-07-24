d1e0615fc5b87dc6f069d51bd7bd54d6cfe202ba
Cristian Medina <cristian.medina94@gmail.com>
2024-07-23 23:13:58 -0300

    arreglo errores en indices del array carrito

--- js/main.js
+++ js/main.js
@@ -1,3 +1,6 @@
+// para restarle a los indices cuando voy a obtener el valor de un array y no pasarme de su longitud real
+const RESTAR_INDICE = 1;
+
 const frutas = [{producto: 'manzana', stock: 10}, {producto: 'pera', stock: 10}, {producto: 'uvas', stock: 10}];
 const almacen = [{producto: 'arroz', stock: 10}, {producto: 'fideos', stock: 10}, {producto: 'galletas', stock: 10}];
 const limpieza = [{producto: 'shampoo', stock: 10}, {producto: 'cepillo de dientes', stock: 10}, {producto: 'desodorante', stock: 10}];
@@ -5,7 +8,7 @@ const limpieza = [{producto: 'shampoo', stock: 10}, {producto: 'cepillo de dient
 // Valido la cantidad ingresada contra el stock que se define por cada producto
 const validarStock = (cant, stock) =>{
     return stock >= cant;
-}
+}   
 
 // Pido por consola el producto a agregar y lo inserto en el array 'carrito'
 const agregarACarrito = (opcion) => {
@@ -15,11 +18,12 @@ const agregarACarrito = (opcion) => {
     switch(opcion){
         case 1:{
             productoElegido = parseInt(prompt('Elija un producto:\n1 - manzana\n2 - pera\n3 - uvas\n4 - Volver\n0 - Terminar'));
+            productoElegido = productoElegido - RESTAR_INDICE; // resto uno para no exceder la longitud del array
             cantidadElegida = parseInt(prompt('Elija una cantidad: '));
             // valido para no agregar más que el stock
             if(validarStock(cantidadElegida, frutas[productoElegido].stock)){
                 // agrego al carrito productos de frutas
-                carrito.push({producto: frutas[productoElegido].producto, cantidadElegida});
+                carrito.push({producto: frutas[productoElegido].producto, stock: cantidadElegida});
                 console.log('Se agrego ' + frutas[productoElegido].producto + ' al carrito...Tenés ' + carrito.length + ' productos en tu carrito.');
             }else{
                 alert('Stock insuficiente!!');
@@ -28,11 +32,12 @@ const agregarACarrito = (opcion) => {
         }
         case 2:{
             productoElegido = parseInt(prompt('Elija un producto:\n1 - arroz\n2 - fideos\n3 - galletas\n4 - Volver\n0 - Terminar'));
+            productoElegido = productoElegido - RESTAR_INDICE; // resto uno para no exceder la longitud del array
             cantidadElegida = parseInt(prompt('Elija una cantidad: '));
             // valido para no agregar más que el stock
             if(validarStock(cantidadElegida, almacen[productoElegido].stock)){
                 // agrego al carrito productos de almacen
-                carrito.push({producto: almacen[productoElegido].producto, cantidadElegida});
+                carrito.push({producto: almacen[productoElegido].producto, stock: cantidadElegida});
                 console.log('Se agrego ' + almacen[productoElegido].producto + ' al carrito...Tenés ' + carrito.length + ' productos en tu carrito.');
             }else{
                 alert('Stock insuficiente!!');
@@ -41,11 +46,12 @@ const agregarACarrito = (opcion) => {
         }
         case 3:{
             productoElegido = parseInt(prompt('Elija un producto:\n1 - shampoo\n2 - cepillo de dientes\n3 - desodorante\n4 - Volver\n0 - Terminar'));
+            productoElegido = productoElegido - RESTAR_INDICE; // resto uno para no exceder la longitud del array
             cantidadElegida = parseInt(prompt('Elija una cantidad: '));
             // valido para no agregar más que el stock
             if(validarStock(cantidadElegida, limpieza[productoElegido].stock)){
                 // agrego al carrito productos de limpieza
-                carrito.push({producto: limpieza[productoElegido].producto, cantidadElegida});
+                carrito.push({producto: limpieza[productoElegido].producto, stock: cantidadElegida});
                 console.log('Se agrego ' + limpieza[productoElegido].producto + ' al carrito...Tenés ' + carrito.length + ' productos en tu carrito.');
             }else{
                 alert('Stock insuficiente!!');
@@ -77,3 +83,15 @@ while(agregar){
     const categoria = parseInt(prompt('Agrega productos a tu carrito de compras:\n1 - Frutas\n2 - Almacen\n3 - Limpieza\n0 - Salir'));
     agregar = agregarACarrito(categoria);
 }
+console.log('Terminaste de agregar productos a tu carrito...');
+
+// Valido y recorro mi array en caso de que no esté vacio
+if(carrito.length > 0){
+    console.log('Tus productos agregados son:');
+    for(let i=0; i < carrito.length; i++){
+        console.log(i+1 + '- ' + carrito[i].producto + ' x ' + carrito[i].stock + ' unidades');
+    }
+}else{
+    console.log('Tu carrito está vacio!');
+}
+
